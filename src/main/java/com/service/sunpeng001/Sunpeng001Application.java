a6a5b759cc2ff29adc9ef92fdd0c64f8f8ab1f80
paas <paas@cse-codegen-backend-4053950211-v12gr>
2017-12-15 06:53:59 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/sunpeng001/Sunpeng001Application.java
@@ -0,0 +1,11 @@
+package com.service.sunpeng001;
+
+import io.servicecomb.foundation.common.utils.BeanUtils;
+import io.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class Sunpeng001Application {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
---
+++ src/main/java/com/service/sunpeng001/controller/Sunpeng001Delegate.java
@@ -0,0 +1,14 @@
+package com.service.sunpeng001.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Sunpeng001Delegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
